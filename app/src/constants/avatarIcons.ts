00771c941187d0f83975c2189534bfe7b9892e4e
JEFFERY XIE <jeffxie27@outlook.com>
2024-03-13 17:17:02 -0600

    Add avatar icons.

---
+++ app/src/constants/avatarIcons.ts
@@ -0,0 +1,35 @@
+import ancientFairyDragon from '../../public/images/ancient-fairy-dragon.png';
+import bigfoot from '../../public/images/bigfoot.png';
+import blueEyesWhiteDragon from '../../public/images/blue-eyes-white-dragon.png';
+import dante from '../../public/images/dante.png';
+import darkMagician from '../../public/images/dark-magician.png';
+import greatShogunShien from '../../public/images/great-shogun-shien.png';
+import hungryBurger from '../../public/images/hungry-burger.png';
+import judgmentDragon from '../../public/images/judgment-dragon.png';
+import pawapuro from '../../public/images/pawapuro.png';
+import pheonixEnforcer from '../../public/images/pheonix-enforcer.png';
+import redEyesBlackDragon from '../../public/images/red-eyes-black-dragon.png';
+import shiEn from '../../public/images/shi-en.png';
+import visasStarfrost from '../../public/images/visas-starfrost.png';
+import weatherPainter from '../../public/images/weather-painter.png';
+import yoko from '../../public/images/yoko.png';
+
+const AvatarIcons = [
+  ancientFairyDragon.src,
+  bigfoot.src,
+  blueEyesWhiteDragon.src,
+  dante.src,
+  darkMagician.src,
+  greatShogunShien.src,
+  hungryBurger.src,
+  judgmentDragon.src,
+  pawapuro.src,
+  pheonixEnforcer.src,
+  redEyesBlackDragon.src,
+  shiEn.src,
+  visasStarfrost.src,
+  weatherPainter.src,
+  yoko.src
+]
+
+export default AvatarIcons;
