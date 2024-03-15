d94106c96b8a6c818b9263d85b5d57ce9471b595
JEFFERY XIE <jeffxie27@outlook.com>
2024-03-15 17:19:30 -0600

    Created constants file for avatar icons and their srcs.

--- app/src/constants/avatarIcons.ts
+++ app/src/constants/avatarIcons.ts
@@ -9,27 +9,72 @@ import judgmentDragon from '../../public/images/judgment-dragon.png';
 import pawapuro from '../../public/images/pawapuro.png';
 import pheonixEnforcer from '../../public/images/pheonix-enforcer.png';
 import redEyesBlackDragon from '../../public/images/red-eyes-black-dragon.png';
-import shiEn from '../../public/images/shi-en.png';
+import shien from '../../public/images/shi-en.png';
 import visasStarfrost from '../../public/images/visas-starfrost.png';
 import weatherPainter from '../../public/images/weather-painter.png';
 import yoko from '../../public/images/yoko.png';
 
-const AvatarIcons = [
-  ancientFairyDragon.src,
-  bigfoot.src,
-  blueEyesWhiteDragon.src,
-  dante.src,
-  darkMagician.src,
-  greatShogunShien.src,
-  hungryBurger.src,
-  judgmentDragon.src,
-  pawapuro.src,
-  pheonixEnforcer.src,
-  redEyesBlackDragon.src,
-  shiEn.src,
-  visasStarfrost.src,
-  weatherPainter.src,
-  yoko.src
-]
+const AvatarIcons = {
+  "ancientFairyDragon": {
+    name: "ancientFairyDragon",
+    icon: ancientFairyDragon.src
+  },
+  "bigfoot": {
+    name: "bigfoot",
+    icon: bigfoot.src
+  },
+  "blueEyesWhiteDragon": {
+    name: "blueEyesWhiteDragon",
+    icon: blueEyesWhiteDragon.src
+  },
+  "dante": {
+    name: "dante",
+    icon: dante.src
+  },
+  "darkMagician": {
+    name: "darkMagician",
+    icon: darkMagician.src
+  },
+  "greatShogunShien": {
+    name: "greatShogunShien",
+    icon: greatShogunShien.src
+  },
+  "hungryBurger": {
+    name: "hungryBurger",
+    icon: hungryBurger.src
+  },
+  "judgmentDragon": {
+    name: "judgmentDragon",
+    icon: judgmentDragon.src
+  },
+  "pawapuro": {
+    name: "pawapuro",
+    icon: pawapuro.src
+  },
+  "pheonixEnforcer": {
+    name: "pheonixEnforcer",
+    icon: pheonixEnforcer.src
+  },
+  "redEyesBlackDragon": {
+    name: "redEyesBlackDragon",
+    icon: redEyesBlackDragon.src
+  },
+  "shien": {
+    name: "shien",
+    icon: shien.src
+  },
+  "visasStarfrost": {
+    name: "visasStarfrost",
+    icon: visasStarfrost.src
+  },
+  "weatherPainter": {
+    name: "weatherPainter",
+    icon: weatherPainter.src
+  },
+  "yoko": {
+    name: "yoko",
+    icon: yoko.src
+  }
+}
 
 export default AvatarIcons;
